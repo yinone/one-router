e823c6e219860246ae5bf95981890dc3e8a0fb06
eleven <eleven.image@gmail.com>
2017-10-31 17:22:22 +0800

    add ava

--- test/index.js
+++ test/index.js
@@ -2,5 +2,6 @@
 * @Author: eleven
 * @Date:   2017-10-30 18:14:59
 * @Last Modified by:   eleven
-* @Last Modified time: 2017-10-30 18:14:59
+* @Last Modified time: 2017-10-31 17:21:55
 */
+
---
+++ test/utils.test.js
@@ -0,0 +1,6 @@
+/*
+* @Author: eleven
+* @Date:   2017-10-31 17:21:16
+* @Last Modified by:   eleven
+* @Last Modified time: 2017-10-31 17:21:16
+*/
